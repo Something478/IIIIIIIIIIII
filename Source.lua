e87f5bacd41fa5de6fdf5ef128db18457810ca55
Something478 <m66361544@gmail.com>
2025-10-17 12:03:34 +0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,1 +1,18 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
 
+getgenv().Syntax = {
+    Version = "1.0.0",
+    Loaded = false
+}
+
+local function LoadModule(name)
+    local url = "https://raw.githubusercontent.com/Something478/IIIIIIIIIIII/main/src/" .. name .. ".lua"
+    return loadstring(game:HttpGet(url))()
+end
+
+local Main = LoadModule("Main")
+local UI = LoadModule("UI")
+local Commands = LoadModule("Commands")
+
+getgenv().Syntax = Main
+Main:Init()
